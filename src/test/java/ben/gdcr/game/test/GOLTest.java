58faea3e6030515a9103cd7f21e0f1c036ca1f8f
Ben Vachon <bvachon@attivio.com>
2018-11-17 11:10:15 -0500

    start from scratch

---
+++ src/test/java/ben/gdcr/game/test/GOLTest.java
@@ -0,0 +1,12 @@
+package ben.gdcr.game.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GOLTest {
+  
+  @Test
+  public void test() {
+    Assert.assertTrue(false);
+  }
+}
